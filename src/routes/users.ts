65acc903a3b2d3a7a62c913f6bb80ac09aabc387
mac <mac@macs-MacBook-Air.local>
2023-01-20 21:30:00 +0100

    add markdown file

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,19 +1,17 @@
-import { requireSignin } from './../middlewares/userAuth';
-import { Router } from "express"
-const router: Router = Router()
+import { requireSignin } from "./../middlewares/userAuth";
+import { Router } from "express";
+const router: Router = Router();
 
+import {
+  createUser,
+  getAllUsers,
+  showUser,
+  deleteUser,
+} from "./../controllers/userController";
 
+router.post("/create", createUser);
+router.get("/all", [requireSignin], getAllUsers);
+router.get("/:id", [requireSignin], showUser);
+router.delete("/delete/:id", [requireSignin], deleteUser);
 
-import { createUser, getAllUsers, showUser, deleteUser } from './../controllers/userController';
-
-
-router.post('/create',createUser)
-router.get('/all',[requireSignin],getAllUsers)
-router.get('/:id',[requireSignin],showUser)
-router.delete('/delete/:id',[requireSignin],deleteUser)
-
-
-
-
-
-export const userRouter: Router = router
+export const userRouter: Router = router;
--- src/tests/models/user.test.ts
+++ src/tests/models/user.test.ts
@@ -1,42 +1,45 @@
 import { User } from "../../models/user";
-const user = new User()
+const user = new User();
 
+describe("Model User", () => {
+  describe("create user", () => {
+    it("add new user to database", async () => {
+      const response = await user.createUser({
+        firstName: "fouad",
+        lastName: "ziko",
+        email: "fouad@email.com",
+        password: "00000",
+      });
+      console.log(response);
+      expect(response);
+    });
+  });
 
-describe('Model User', () => {
+  describe("get Users", () => {
+    it("retrieve Users list", () => {
+      const response = user.gestUsers();
+      expect(response);
+    });
+  });
 
-   describe('create user', () => {
-    it('add new user to database', () => {
-        const response = user.createUser({firstName:"fouad", lastName:"ziko", email: "fouad@email.com", password: "00000"})
-            expect(response)
-    })
-   })
+  describe("user by id", () => {
+    it("retrieve User by id", () => {
+      const response = user.userById(1);
+      expect(response);
+    });
+  });
 
-   describe('get Users', () => {
-    it('retrieve Users list', () => {
-        const response = user.gestUsers()
-        expect(response)
-    })
-   })
+  describe("user delete", () => {
+    it("delete user from database", () => {
+      const response = user.userDelete(1);
+      expect(response);
+    });
+  });
 
-   describe('user by id', () => {
-    it('retrieve User by id', () => {
-        const response = user.userById(1)
-        expect(response)
-    })
-   })
-
-   describe('user delete', () => {
-    it('delete user from database', () => {
-        const response = user.userDelete(1)
-        expect(response)
-    })
-   })
-   
-   describe('hash password ', () => {
-    it('hash_password', () => {
-        const response = user.hash_password("0000")
-        expect(response)
-    })
-   })
-
-})
+  describe("hash password ", () => {
+    it("hash_password", () => {
+      const response = user.hash_password("0000");
+      expect(response);
+    });
+  });
+});
